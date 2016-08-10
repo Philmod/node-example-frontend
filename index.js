64838d8087188af5f1bd4d11682c5228c521f8d1
Philmod <philippe.modard@gmail.com>
2016-08-09 23:33:53 -0400

    add status page

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var app = require('express')();
+var serverStatus = require('express-server-status');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use('/status', serverStatus(app));
 
 app.listen(3001, function() {
   console.log('listening on *:3001');
