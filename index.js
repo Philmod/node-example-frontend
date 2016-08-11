cab1f802dd5c9f53d06427c2a183c1759c58205c
Philmod <philippe.modard@gmail.com>
2016-08-10 21:45:17 -0400

    Pass hostname to the html

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var app = require('express')();
+var os = require('os');
 var morgan = require('morgan');
+var exphbs = require('express-handlebars');
 var serverStatus = require('express-server-status');
 
 app.use(morgan('dev'));
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render(__dirname + '/index.hbs', {
+    htmlserver: os.hostname()
+  });
 });
 
 app.use('/status', serverStatus(app));
