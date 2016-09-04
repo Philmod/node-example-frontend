2fcbdf3c74014cbc9b54ee768c45a62c2cecf65a
Philmod <philippe.modard@gmail.com>
2016-09-03 22:10:53 -0400

    add some routes to test kubernetes

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.get('/', function(req, res) {
 
 app.use('/status', serverStatus(app));
 
+// The next routes are used to test kubernetes.
+app.get('/break', function(req, res) {
+  while (true) {}
+});
+app.get('/slow', function(req, res) {
+  var delay = req.query.delay || 1000;
+  setTimeout(function() {
+    return res.sendStatus(200);
+  }, delay);
+});
+
 app.listen(3001, function() {
   console.log('listening on *:3001');
 });
