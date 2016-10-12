93cc0eb4a2ec8306589c87ee701adc32c83cbccf
Philmod <philippe.modard@gmail.com>
2016-10-11 21:46:21 -0400

    Trick for the kubernetes service-loadbalancer which base url for this app is frontend/

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/public'));
 app.use(express.static('public'));
+app.use('/frontend', express.static('public'));
 
 app.get('/', function(req, res) {
   res.render('index.hbs', {
